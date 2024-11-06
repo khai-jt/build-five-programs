8c051817075249814a902d21d1297636db99a9aa
Khairunnisa Johan Thani <khai.jt@outlook.com>
2024-11-06 04:35:43 +0000

    Save progress: 326.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-echo -e \n"~~ Questionnaire ~~"\n
+echo -e "\n~~ Questionnaire ~~\n"
 
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
 
 echo $QUESTION1
-
 read NAME
 
-echo "Hello $NAME."
-
 echo $QUESTION2
-
 read LOCATION
 
-echo "Hello $NAME from $LOCATION."
-
+echo Hello $NAME from $LOCATION.
