946367ead2c664cc396cafd57cadd3b752c81bb3
Khairunnisa Johan Thani <khai.jt@outlook.com>
2024-11-09 09:06:18 +0000

    Save progress: 1260.1

--- fortune.sh
+++ fortune.sh
@@ -8,11 +8,13 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 
 GET_FORTUNE() {
-  echo "Ask a yes or no question:"
+  echo Ask a yes or no question:
 }
 
-GET_FORTUNE
+until [[ $QUESTION == test? ]]
+do
+  GET_FORTUNE
+  read QUESTION
+done
 
 echo ${RESPONSES[$N]}
-
-read QUESTION
